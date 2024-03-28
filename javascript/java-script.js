88e6e92e30bb79ec895b05efbdddaa8079ffc6de
chukalka <145771465+chukalka@users.noreply.github.com>
2024-03-28 20:04:30 +0300

    Add files via upload

---
+++ javascript/java-script.js
@@ -0,0 +1,139 @@
+$ (document) .ready(function() {
+    $( ".cr1" ).draggable();
+    $( ".cr2" ).draggable();
+    $( ".cr3" ).draggable();
+    $( ".cr4" ).draggable();
+    $( ".cr5" ).draggable();
+    $( ".cr6" ).draggable();
+
+    $( ".semagr" ).draggable();
+    $( ".semagr2" ).draggable();
+    $( ".semagr3" ).draggable();
+    $( ".semagr4" ).draggable();
+
+    $( ".p0" ).draggable();
+    $( ".p1" ).draggable();
+    $( ".p2" ).draggable();
+    $( ".p3" ).draggable();
+    $( ".p4" ).draggable();
+    $( ".p5" ).draggable();
+    $( ".p6" ).draggable();
+    $( ".p7" ).draggable();
+    $( ".p8" ).draggable();
+    $( ".p9" ).draggable();
+    $( ".p10" ).draggable();
+    $( ".p11" ).draggable();
+
+    $( ".l1" ).draggable();
+    $( ".l2" ).draggable();
+    $( ".l3" ).draggable();
+    $( ".l4" ).draggable();
+    $( ".l5" ).draggable();
+    $( ".l6" ).draggable();
+
+    $( ".cr11" ).draggable();
+    $( ".cr22" ).draggable();
+    $( ".cr33" ).draggable();
+
+    $( ".sema11" ).draggable();
+    $( ".sema12" ).draggable();
+    $( ".sema13" ).draggable();
+
+    $( function() {
+        $( "#one2flex" ).sortable();
+        $( "#one2flex" ).disableSelection();
+    } );
+
+    $(".lov1").click(function(){
+      $(this).toggle();
+    });
+
+    $(".lov2").click(function(){
+      $(this).toggle();
+    });
+
+    $(".lov3").click(function(){
+      $(this).toggle();
+    });
+
+    $(".lov4").click(function(){
+      $(this).toggle();
+    });
+
+    $(".lov5").click(function(){
+      $(this).toggle();
+    });
+
+    $(".lov6").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s1").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s2").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s3").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s4").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s5").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s6").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s7").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s8").click(function(){
+      $(this).toggle();
+    });
+
+    $(".s9").click(function(){
+      $(this).toggle();
+    });
+  
+
+    // $('.lov1').on("click", function(){
+    //   $('.lov1').toggle();
+    // });
+
+    // $(".lov1").click(function(){
+    //   $(this).stop();
+    // });
+    
+    // $('#btn1').on("click", function(){
+    //   $('#text').toggle(); 
+    // });
+
+});
+//   $ (document) .ready(function() {
+//     $( ".cr2" ).draggable();
+//   });
+
+//   $ (document) .ready(function() {
+//     $( ".cr3" ).draggable();
+//   });
+
+//   $ (document) .ready(function() {
+//     $( ".cr4" ).draggable();
+//   });
+
+//   $ (document) .ready(function() {
+//     $( ".cr5" ).draggable();
+//   });
+
+//   $ (document) .ready(function() {
+//     $( ".cr6" ).draggable();
+//   });
